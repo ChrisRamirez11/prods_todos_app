6b5a0c6993f1df55e78de9d20a3308dcea15b08a
ChrisCastanedaR <101981955+ChrisRamirez11@users.noreply.github.com>
2024-11-18 16:07:08 -0500

    overflow fixed

--- lib/widgets/custom_loader_widget.dart
+++ lib/widgets/custom_loader_widget.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
 
 Widget customLoaderWidget() {
-  return Container(
-    decoration: const BoxDecoration(boxShadow: [BoxShadow(color: Colors.black26)]),
-    width: double.infinity,
-    height: double.infinity,
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Container(
-            padding: const EdgeInsets.all(50),
-            child: Image.asset('assets/images/loading.png')),
-        const CircularProgressIndicator()
-      ],
+  return Center( // Centra todo el contenido en la pantalla
+    child: Container(
+      decoration: const BoxDecoration(boxShadow: [BoxShadow(color: Colors.black26)]),
+      width: 300, 
+      height: 300, 
+      child: SingleChildScrollView( 
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20), 
+              constraints: const BoxConstraints(
+                maxHeight:300, 
+                maxWidth: 300, 
+              ),
+              child: Image.asset('assets/images/loading.png', fit: BoxFit.contain),
+            ),
+            const SizedBox(height: 20), 
+            const CircularProgressIndicator(),
+          ],
+        ),
+      ),
     ),
   );
 }
